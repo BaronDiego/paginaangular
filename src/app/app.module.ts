8293e6ca821d6f364f1e32652a21e079ee426f8b
Diego.Baron <diegoivanbaron@gmail.com>
2017-08-10 11:09:57 -0500

    Hasta About

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { FooterComponent } from './componentes/footer/footer.component';
 import { PortafolioComponent } from './componentes/portafolio/portafolio.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { ProductoComponent } from './componentes/producto/producto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    ProductoComponent
   ],
   imports: [
     BrowserModule
